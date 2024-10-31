4c848981cd63ccda53ee8a1f2aed122244144c91
rahma <fijriatirahmaturrizqi@gmail.com>
2024-10-31 07:02:01 +0700

    modifikasi percobaan 1

--- ArrayBilangan10.java
+++ ArrayBilangan10.java
@@ -1,15 +1,19 @@
 public class ArrayBilangan10 {
     public static void main(String[] args) {
-        int[] bil = new int[4];
+        // int[] bil = new int[4];
 
-        bil[0] = 5;
-        bil[1] = 13;
-        bil[2] = -7;
-        bil[3] = 17;
+        // bil[0] = 5;
+        // bil[1] = 13;
+        // bil[2] = -7;
+        // bil[3] = 17;
 
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+        // System.out.println(bil[0]);
+        // System.out.println(bil[1]);
+        // System.out.println(bil[2]);
+        // System.out.println(bil[3]);
+        int[] bil = { 5, 13, -7, 17 };
+        for (int i = 0; i < 4; i++) {
+            System.out.println(bil[i]);
+        }
     }
 }
