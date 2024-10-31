96a36856fa53d15bce23dbc69c0a055920609c56
rahma <fijriatirahmaturrizqi@gmail.com>
2024-10-31 07:40:55 +0700

    modifikasi percobaan 3 menampilkan jumlah mahasiswa lulus

--- ArrayRataNilai10.java
+++ ArrayRataNilai10.java
@@ -7,16 +7,15 @@ public class ArrayRataNilai10 {
         int[] nilaiMhs = new int[10];
         double total = 0;
         double rata2;
+        int jmlLulus = 0;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
             nilaiMhs[i] = sc.nextInt();
+            if (nilaiMhs[i] > 70) {
+                jmlLulus++;
+            }
         }
-        for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
-        }
-
-        rata2 = total / nilaiMhs.length;
-        System.out.println("rata-rata nilai = " + rata2);
+        System.out.println("jumlah mahasiswa yang lulus: " + jmlLulus);
     }
 }
