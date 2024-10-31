04633fcca089f3cc4ba7a0fdce6bcae17360b363
rahma <fijriatirahmaturrizqi@gmail.com>
2024-10-31 07:57:59 +0700

    modifikasi percobaan 3 input jumlah mahasiswa, rata2 lulus, rata2 tidak lulus

--- ArrayRataNilai10.java
+++ ArrayRataNilai10.java
@@ -4,18 +4,45 @@ public class ArrayRataNilai10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] nilaiMhs = new int[10];
-        double total = 0;
-        double rata2;
-        int jmlLulus = 0;
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jumlahMahasiswa = sc.nextInt();
 
-        for (int i = 0; i < nilaiMhs.length; i++) {
+        if (jumlahMahasiswa <= 0) {
+            System.out.println("Jumlah mahasiswa harus lebih dari 0.");
+            return;
+        }
+
+        int[] nilaiMhs = new int[jumlahMahasiswa];
+        int[] arrayLulus = new int[jumlahMahasiswa];
+        int[] arrayGagal = new int[jumlahMahasiswa];
+
+        int lulus = 0, gagal = 0;
+        double totalLulus = 0, totalTidakLulus = 0;
+
+        for (int i = 0; i < jumlahMahasiswa; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
             nilaiMhs[i] = sc.nextInt();
+
             if (nilaiMhs[i] > 70) {
-                jmlLulus++;
+                lulus++;
+                arrayLulus[i] = nilaiMhs[i];
+                totalLulus += nilaiMhs[i];
+            } else {
+                gagal++;
+                arrayGagal[i] = nilaiMhs[i];
+                totalTidakLulus += nilaiMhs[i];
             }
         }
-        System.out.println("jumlah mahasiswa yang lulus: " + jmlLulus);
+
+        if (lulus > 0) {
+            double rataLulus = totalLulus / lulus;
+            System.out.println("Rata-rata nilai mahasiswa lulus adalah: " + rataLulus);
+        }
+
+        if (gagal > 0) {
+            double rataGagal = totalTidakLulus / gagal;
+            System.out.println("Rata-rata nilai mahasiswa tidak lulus adalah: " +
+                    rataGagal);
+        }
     }
 }
