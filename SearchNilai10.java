37fb1709f02569289e1662fd07c32fb50f9a5736
rahma <fijriatirahmaturrizqi@gmail.com>
2024-10-31 08:13:00 +0700

    modifikasi input banyak elemen, isi array, input key, cetak hasil cari

--- SearchNilai10.java
+++ SearchNilai10.java
@@ -1,17 +1,29 @@
+import java.util.Scanner;
+
 public class SearchNilai10 {
     public static void main(String[] args) {
-        int[] arrNilai = { 80, 85, 78, 96, 90, 82, 86 };
-        int key = 90;
+        Scanner sc = new Scanner(System.in);
+
+        int[] arrNilai;
+        System.out.print("masukkan banyaknya nilai yang akan diinput: ");
+        int jmlElemen = sc.nextInt();
+        arrNilai = new int[jmlElemen];
         int hasil = 0;
 
         for (int i = 0; i < arrNilai.length; i++) {
-            if (key == arrNilai[i]) {
+            System.out.print("masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            arrNilai[i] = sc.nextInt();
+        }
+        System.out.print("masukkan nilai yang ingin dicari: ");
+        int key = sc.nextInt();
+        for (int i = 0; i < arrNilai.length; i++) {
+            if (arrNilai[i] == key) {
                 hasil = i;
                 break;
             }
         }
         System.out.println();
-        System.out.println("Nilai " + key + " ketemu di indeks ke-" + hasil);
+        System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + (hasil + 1));
         System.out.println();
     }
 }
